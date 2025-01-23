f2a059bccb17debab3f58ac22492d270b5be6829
ITakora <oyamitakora@gmail.com>
2025-01-23 09:06:16 +0700

    adding todo textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo/ui/screens/home_screen.dart';
+import 'package:todo/ui/screens/todo_field_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const TodoFieldScreen(),
     );
   }
 }
--- lib/ui/screens/todo_field_screen.dart
+++ lib/ui/screens/todo_field_screen.dart
@@ -1,3 +1,4 @@
+import 'package:day_night_time_picker/day_night_time_picker.dart';
 import 'package:flutter/material.dart';
 
 class TodoFieldScreen extends StatefulWidget {
@@ -8,8 +9,64 @@ class TodoFieldScreen extends StatefulWidget {
 }
 
 class _TodoFieldScreenState extends State<TodoFieldScreen> {
+  Time _time = Time(hour: 00, minute: 00, second: 00);
+
+  void onTimeChanged(Time newTime) {
+    setState(() {
+      _time = newTime;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: const ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.grey)),
+              child: const Text(
+                "Simpan",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+              decoration: InputDecoration(
+            hintText: "Judul Todo",
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+          )),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              showPicker(
+                  context: context,
+                  value: _time,
+                  moonAsset: Image.asset('assets/images/moon.png'),
+                  sunAsset: Image.asset("assets/images/sun.png"),
+                  sunrise: TimeOfDay(hour: 6, minute: 0), // optional
+                  sunset: TimeOfDay(hour: 18, minute: 0), // optional
+                  duskSpanInMinutes: 120, // optional
+                  onChange: onTimeChanged,
+                  is24HrFormat: true),
+            );
+          },
+          child: Text(
+            "Open time picker",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ]),
+    );
   }
 }
